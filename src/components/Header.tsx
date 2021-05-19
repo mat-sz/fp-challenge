6df6d5b93cc29af2aaf41209f7ef56b66fbd0971
Mat Sz <contact@matsz.dev>
2021-05-19 18:17:25 +0200

    fixed responsiveness

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
 
 export const Header = styled.div`
-  grid-column: 1 / 3;
+  grid-column: 1 / 2;
+
+  @media screen and (min-width: 1000px) {
+    grid-column: 1 / 3;
+  }
 `;
